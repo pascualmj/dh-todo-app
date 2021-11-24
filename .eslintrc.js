c005944d15fe7d14e57b15eaa2e15b205e5d1077
Manuel Pascual <mjpascual@teco.com.ar>
2021-11-24 02:00:01 -0300

    fix: disable eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   settings: {
     react: {
